2385d5a8fbe307263f71885ae4ae959f450f9b76
ChirenOnHive <44252697+chirenonhive@users.noreply.github.com>
2025-02-08 13:58:55 -0500

    fix

--- src/server/index.js
+++ src/server/index.js
@@ -24,6 +24,7 @@ async function startServer() {
         // Basic middleware setup
         app.use(cors());
         app.use(express.json());
+        app.use(express.static(path.join(__dirname, '../web/public')));
         app.use(express.urlencoded({ extended: true }));
         app.use(morgan('dev'));
 
